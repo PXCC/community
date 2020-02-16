86d94f0579c29206e3f2f23b18f7505e2923987d
PXCC <717208789@qq.com>
2020-02-16 22:07:24 +0800

    qusetion

---
+++ src/main/java/com/community2/controller/PublishController.java
@@ -0,0 +1,74 @@
+package com.community2.controller;
+
+import com.community2.mapper.QuestionMapper;
+import com.community2.mapper.UserMapper;
+import com.community2.model.Question;
+import com.community2.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class PublishController {
+
+    @Autowired
+    private QuestionMapper questionMapper;
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/publish")
+    public String publish(){
+
+        return "publish";
+    }
+    @PostMapping("/publish")
+    public String doPublish(
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("tag") String tag,
+            HttpServletRequest request,
+            Model model
+    ){
+
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        if(cookies == null){
+            model.addAttribute("error","用户未登录");
+            return "publish";
+        }else {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
+        if (user == null){
+            model.addAttribute("error","用户未登录");
+            return "publish";
+        }
+
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(System.currentTimeMillis());
+
+
+        questionMapper.create(question);
+
+        return "redirect:/";
+    }
+}
